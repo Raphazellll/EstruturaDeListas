d8cb70b5986c11387d74cdb558eb65ee3dd0a7fb
Raphazelll <pimentaraphael000@gmail.com>
2025-01-25 20:25:25 -0300

    atualizando aula de nó duplamente encadeado e trocando nome de outros exercicios.

--- src/one/dio/No.java
+++ src/one/dio/No.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation;
+package one.dio;
 
 public class No<T> {
 
---
+++ src/one/listaduplamenteencadeada/DuplamenteEncadeada.java
@@ -0,0 +1,47 @@
+package one.listaduplamenteencadeada;
+
+public class DuplamenteEncadeada<T> {
+
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+
+    private int tamanhoLista;
+
+    public DuplamenteEncadeada() {
+        primeiroNo = null;
+        ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    public void add(T elemento) {
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(null);
+        novoNo.setNoPrevio(ultimoNo);
+        if (primeiroNo == null) {
+            primeiroNo = novoNo;
+        }
+        if (ultimoNo != null) {
+            ultimoNo.setNoProximo(novoNo);
+        }
+        ultimoNo = novoNo;
+        tamanhoLista++;
+    }
+
+    private NoDuplo<T> getNo(int index) {
+
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
+    public int size() {
+        return this.tamanhoLista;
+    }
+
+}
