b0fba8c84d78d64def45a1b9b415547c0c52523e
Raphazelll <pimentaraphael000@gmail.com>
2025-01-25 21:01:03 -0300

    ultimo commit do dia. vou jogar com minha esposa hahah

--- src/one/listaduplamenteencadeada/DuplamenteEncadeada.java
+++ src/one/listaduplamenteencadeada/DuplamenteEncadeada.java
@@ -31,6 +31,47 @@ public class DuplamenteEncadeada<T> {
         tamanhoLista++;
     }
 
+    public void add(int index, T elemento) {
+        NoDuplo<T> noAuxiliar = getNo(index);
+        NoDuplo<T> novoNo = new NoDuplo<>(elemento);
+        novoNo.setNoProximo(noAuxiliar);
+
+        if (novoNo.getNoProximo() != null) {
+            novoNo.setNoPrevio(noAuxiliar.getNoPrevio());
+            novoNo.getNoProximo().setNoPrevio(novoNo);
+        } else {
+            novoNo.setNoPrevio(ultimoNo);
+            ultimoNo = novoNo;
+        }
+
+        if (index == 0) {
+            primeiroNo = novoNo;
+        } else {
+            novoNo.getNoPrevio().setNoProximo(novoNo);
+        }
+
+        tamanhoLista++;
+
+    }
+
+    public void remove(int index) {
+        if (index == 0) {
+            primeiroNo = primeiroNo.getNoProximo();
+            if (primeiroNo != null) {
+                primeiroNo.setNoPrevio(null);
+            }
+        } else {
+            NoDuplo<T> noAuxiliar = getNo(index);
+            noAuxiliar.getNoPrevio().setNoProximo(noAuxiliar.getNoProximo());
+            if (noAuxiliar != ultimoNo) {
+                noAuxiliar.getNoProximo().setNoPrevio(noAuxiliar.getNoPrevio());
+            } else {
+                ultimoNo = noAuxiliar;
+            }
+        }
+        this.tamanhoLista--;
+    }
+
     private NoDuplo<T> getNo(int index) {
 
         NoDuplo<T> noAuxiliar = primeiroNo;
@@ -42,6 +83,20 @@ public class DuplamenteEncadeada<T> {
 
     public int size() {
         return this.tamanhoLista;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+
+        NoDuplo<T> noAuxiliar = primeiroNo;
+        for (int i = 0; i < size(); i++) {
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() + "}]--->";
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        strRetorno += "null";
+        return strRetorno;
     }
 
 }
